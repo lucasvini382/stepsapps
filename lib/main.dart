585a9e20da83c1e040d24d245a81ab2445cb65cf
Lucas Oliveira <lucas.sales@justa.com.vc>
2022-04-09 14:19:23 -0300

    feat: :sparkles: cria StepForm.

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Step App',
+      home: StepForm(),
+    );
+  }
+
+}
